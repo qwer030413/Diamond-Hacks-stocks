1b8077e5347edb3a9a74e6c182c26e470153c193
qwer030413 <comosjay@gmail.com>
2025-04-05 15:49:43 -0700

    set up router

---
+++ frontend/src/pages/History.tsx
@@ -0,0 +1,9 @@
+export default function History() {
+  return (
+    <div className="history">
+      <h1>History Page</h1>
+      <p>This page displays the history of your interactions.</p>
+      <p>Here you can review past activities and events.</p>
+    </div>
+  );
+}
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home() {
+  return (
+    <div className="home">
+      <h1>Welcome to the Home Page!</h1>
+      <p>This is the main page of our application.</p>
+      <p>Feel free to explore the features and functionalities.</p>
+    </div>
+  );
+}
