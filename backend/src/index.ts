84fafcb296ba8aab27dd795978d17527b3158281
qwer030413 <comosjay@gmail.com>
2025-04-06 07:40:49 -0700

    set up database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import type { Request, Response } from 'express';
 import cors from 'cors';
-
+import { Client } from 'pg';
 const app = express();
 const PORT = 3000;
 let balance = 10000;
@@ -11,6 +11,16 @@ app.use(
       origin: 'http://localhost:5175',
     }),
   );
+const client = new Client({
+  user: 'postgres', // Replace with your PostgreSQL username
+  host: 'localhost',
+  database: 'postgres', // Replace with your PostgreSQL database name
+  password: 'qwer', // Replace with your PostgreSQL password
+  port: 5432, // Default PostgreSQL port
+});
+client.connect().then(() => {
+  console.log('Connected to PostgreSQL database!');
+})
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello from TypeScript + Express!');
 });
